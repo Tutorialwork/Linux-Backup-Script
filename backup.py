823605094b317d54cce4ffce615b4272ca431f50
Tutorialwork <tutorialworktv@gmail.com>
2021-05-23 11:13:15 +0200

    implement custom date/file formate & add support for macOS

--- backup.py
+++ backup.py
@@ -5,11 +5,12 @@ from sys import platform
 import config
 from logger import Logger
 
-date = time.strftime("%d-%m-%Y_%H-%M")
+date = time.strftime(config.date_format)
 logger = Logger("backup.log")
 
+# This function check if the script is running on a unix based operating system.
 def os_check():
-    if platform == "linux" or platform == "linux2":
+    if platform.startswith('linux') or platform == 'darwin':
         return True
     else:
         return False
@@ -17,16 +18,17 @@ def os_check():
 
 def mysql_backup():
     logger.log("INFO", "Starting backup for MySQL database...")
-    tmpCheck = os.system("cd "+config.cloud_mount)
+    tmpCheck = os.system("cd "+config.backup_location)
     if tmpCheck == 0:
-        createBackup = os.system("cd " + config.cloud_mount + " && "
-                                 "mysqldump -u " + config.mysql_user + " -p'" + config.mysql_password + "' --all-databases > mysqlbackup-" + date + ".sql")
+        mysqlBackupFileName = getFileName('mysqlBackup') + '.sql'
+        createBackup = os.system("cd " + config.backup_location + " && "
+                                 "mysqldump -u " + config.mysql_user + " -p'" + config.mysql_password + "' --all-databases > " + mysqlBackupFileName)
 
         if createBackup == 0:
             logger.log("SUCCESS", "MySQL database backup successfully created")
         else:
             logger.log("ERROR", "MySQL database backup failed")
-            os.system("cd " + config.cloud_mount + " && rm mysqlbackup-" + date + ".sql")
+            os.system("cd " + config.backup_location + " && rm mysqlbackup-" + date + ".sql")
     else:
         logger.log("ERROR", "Mount not exits")
 
@@ -38,9 +40,10 @@ def backup():
     else:
         counter = 0
         for dir in config.backup_dirs:
+            backupFileName = getFileName('backup_' + str(counter + 1)) + '.tar.gz'
             counter += 1
             logger.log("INFO", "Starting backup for "+dir+"...")
-            status = os.system("cd " + config.cloud_mount + " && tar -czvf backup_" + str(counter) + "-" + date + ".tar.gz "+dir)
+            status = os.system("cd " + config.backup_location + " && tar -czvf " + backupFileName + " " + dir)
             if status == 0:
                 logger.log("SUCCESS", "Backup for "+dir+" successfully")
             else:
@@ -49,7 +52,7 @@ def backup():
 
 def clearBackups():
     logger.log("INFO", "Cleaning backup dir...")
-    for file in os.listdir(config.cloud_mount):
+    for file in os.listdir(config.backup_location):
         if os.path.isfile(file):
             os.remove(file)
         elif os.path.isdir(file):
@@ -58,8 +61,18 @@ def clearBackups():
             logger.log("ERROR", file + " can't be deleted!")
     logger.log("SUCCESS", "Successfully deleted old backup files")
 
+def getFileName(name):
+    fileName = config.backup_name_format
+
+    fileName = fileName.replace('%date%', date)
+    fileName = fileName.replace('%backupName%', name)
+
+    print(fileName)
+
+    return fileName
+
 if os_check():
-    if os.path.exists(config.cloud_mount):
+    if os.path.exists(config.backup_location):
         if config.clear_backups:
             clearBackups()
         if config.mysql_backup:
@@ -67,8 +80,8 @@ if os_check():
         backup()
     else:
         logger.log("ERROR", "Failed to create backup")
-        logger.log("ERROR", config.cloud_mount + " not exists")
+        logger.log("ERROR", config.backup_location + " not exists")
     logger.closeFile()
 else:
-    logger.log("ERROR", "Sorry but this script is only for Linux")
+    logger.log("ERROR", "Sorry but this script is only for Linux or macOS")
     logger.closeFile()
